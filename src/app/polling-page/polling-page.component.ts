6c3c44fc8ee7125e8e637900162221c44f23a7ec
Indu04 <indu.104@gmail.com>
2018-04-03 21:32:16 +0530

    Added show functions based on time

--- src/app/polling-page/polling-page.component.ts
+++ src/app/polling-page/polling-page.component.ts
@@ -2,16 +2,26 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatCardModule} from '@angular/material/card';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-polling-page',
   templateUrl: './polling-page.component.html',
   styleUrls: ['./polling-page.component.css']
 })
-export class PollingPageComponent {
+export class PollingPageComponent implements OnInit{
 
-  url = 'assets/';
+  ngOnInit(){
+    Observable.interval(1000 * 60).subscribe(x => {this.showM1(this.matchInfo.Time); this.showM2(this.matchInfo2.Time)});
+  }
+
   matchInfo = {team1:'RCB', team2:'CSK', Venue: 'Bangalore' ,Date: '24th April 2018', Time: '16:00'};
+  
+  showPollM1 = true;
+  showPollM2 = true;
+  url = 'assets/';
+
 
   form = new FormGroup({
       name: new FormControl('',[Validators.required,]), // Default validation
@@ -19,7 +29,8 @@ export class PollingPageComponent {
   },this.validateMember);
 
   validateMember(frm: FormGroup) {
-    if( ['Sachin', 'Amit', 'Selvi', 'Viji', 'Vivek', 'Vijay', 'Rakesh', 'Indu', 'Raks', 'Pawan', 'Satish', 'Divs'].indexOf(frm.get('name').value) === -1) {
+    var membersList = ['Sachin', 'Amit', 'Selvi', 'Viji', 'Vivek', 'Vijay', 'Rakesh', 'Indu', 'Raks', 'Pawan', 'Satish', 'Divs'];
+    if( membersList.indexOf(frm.get('name').value) === -1) {
       console.log('false');
       frm.get('name').setErrors( {validateMember: true} )}
       else {
@@ -38,8 +49,8 @@ export class PollingPageComponent {
   }
 
 // ############################################################################################################
-  matchInfo2 = {team1:'DD', team2:'RR', Venue: 'Delhi' ,Date: '24th April 2018', Time: '20:00'};
-  // matchInfo2 = null
+  // matchInfo2 = {team1:'DD', team2:'RR', Venue: 'Delhi' ,Date: '24th April 2018', Time: '20:00'};
+  matchInfo2 = null;
 
   form2 = new FormGroup({
       name2: new FormControl('',[Validators.required,]), // Default validation
@@ -47,7 +58,8 @@ export class PollingPageComponent {
   },this.validateMember2);
 
   validateMember2(frm2: FormGroup) {
-    if( ['Sachin', 'Amit', 'Selvi', 'Viji', 'Vivek', 'Vijay', 'Rakesh', 'Indu', 'Raks', 'Pawan', 'Satish', 'Divs'].indexOf(frm2.get('name2').value) === -1) {
+    var membersList = ['Sachin', 'Amit', 'Selvi', 'Viji', 'Vivek', 'Vijay', 'Rakesh', 'Indu', 'Raks', 'Pawan', 'Satish', 'Divs'];
+    if( membersList.indexOf(frm2.get('name2').value) === -1) {
       console.log('false');
       frm2.get('name2').setErrors( {validateMember2: true} )}
       else {
@@ -62,11 +74,40 @@ export class PollingPageComponent {
   submitValue2(){
     console.log(this.form2.get('name2').value);
     console.log(this.form2.get('selectTeam2').value);
+    var d = new Date();
+    console.log(d.getHours()-1);
+    console.log(d.getMinutes());
   }
 
   public getUrl(team: string): string {
     return this.url.concat(team).concat('.png');
   }
 
-}
+  public showM1(matchTime:string):any{
+          
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes(); //keeping this for debugging
+    var matchHour = +matchTime.split(':')[0]-1
 
+    if (hour >= matchHour){
+      this.showPollM1 = null;
+    }
+  console.log(matchHour, this.showPollM1, min, hour)
+
+  }
+
+  public showM2(matchTime:string):any{
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes(); //keeping this for debugging
+    var matchHour = +matchTime.split(':')[0]-1
+
+    if (hour >= matchHour){
+      this.showPollM2 = null;
+    }
+  console.log(matchHour, this.showPollM2, min, hour) //keeping this for debugging
+  
+    }
+
+}
